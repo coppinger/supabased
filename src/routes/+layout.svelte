3fc3605415a56c82bb5a4fc0a9f322f267b70433
Charlie Coppinger <charles.coppinger@gmail.com>
2024-04-20 15:33:59 +1200

    get in combo route and update shadcn styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	import '../app.pcss';
 </script>
 
-<div
-	class="min-w-screen dark flex min-h-screen flex-col items-center justify-center gap-4 bg-black text-[#00ff00]"
->
+<div class="">
 	<slot />
 </div>
