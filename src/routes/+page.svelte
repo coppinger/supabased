173a1d3a9732dd98ab5958a049b508b814cb4a70
ahmad1702 <ahmad1702@gmail.com>
2024-04-21 22:50:07 -0500

    4pm progress ahmad

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import Supa3D from '$lib/components/Supa3D.svelte';
 	import Profile from '$lib/components/profile/profile.svelte';
+	import { Input } from '$lib/components/ui/input';
 	import Filter from '$lib/filter/filter.svelte';
 	import { createProfilesState } from '$lib/stores/profiles.js';
+	import Fuse from 'fuse.js';
 	import { Box, Cloud, Database, Lock, MousePointerClick, Triangle } from 'lucide-svelte';
+	import { MagnifyingGlass } from 'phosphor-svelte';
 	export let data;
-
-	$: ({ profiles, filter } = createProfilesState(data.profiles, data.supabase));
 
 	const tags = [
 		{
@@ -34,6 +35,57 @@
 			Icon: Box
 		}
 	];
+	$: ({ profiles, filter } = createProfilesState(data.profiles, data.supabase));
+
+	let profileSearchTerm = '';
+
+	$: fuse = new Fuse($profiles, {
+		keys: [
+			'projects',
+			'github',
+			'twitter',
+			'availabilities',
+			'endorsement_num',
+			'description',
+			'country',
+			'city',
+			'devType',
+			'profile_pic',
+			'lastName',
+			'firstName',
+			'projects.name',
+			'projects.url',
+			'projects.usedTech',
+			'projects.description',
+			'projects.github',
+			'projects.link'
+		],
+		threshold: 0.3
+	});
+
+	$: searchedFilteredProfiles = profileSearchTerm
+		? fuse.search(profileSearchTerm).map((res) => res.item)
+		: $profiles;
+
+	$: filteredProfiles = $profiles.filter((profile) => {
+		return true;
+		if (
+			$filter.availibility.length &&
+			!profile.availabilities.some((availability) => $filter.availibility.includes(availability))
+		)
+			return false;
+
+		if (
+			!profile.projects
+				.map((project) =>
+					project.usedTech.some((experience) => $filter.experiences.includes(experience))
+				)
+				.flat()
+				.some((bool) => bool)
+		)
+			return false;
+		return true;
+	});
 </script>
 
 <div class="">
@@ -58,11 +110,15 @@
 				<Supa3D speed={8} />
 			</div>
 		</div>
-		<div class="container px-2 mx-auto w-full flex gap-4">
+		<div class="container px-2 mx-auto w-full grid grid-cols-3 gap-4 h-auto items-start">
 			<Filter {filter} />
 			<!-- TODO added ml-auto class for now, remove when we add filter/search if not needed-->
-			<div class="w-2/3 ml-auto space-y-4">
-				{#each $profiles as profile}
+			<div class="flex-1 space-y-4 col-span-2">
+				<span class="relative">
+					<MagnifyingGlass class="absolute left-4 top-1/2 -translate-y-1/2" />
+					<Input bind:value={profileSearchTerm} placeholder="Search Profiles" class="pl-10" />
+				</span>
+				{#each filteredProfiles as profile}
 					<Profile {profile} />
 				{/each}
 			</div>
