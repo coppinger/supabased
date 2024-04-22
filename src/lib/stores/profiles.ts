1d31b5a18bf3627b2cc003f90a86fc77044a3c69
Ahmad Sandid <ahmad1702@gmail.com>
2024-04-22 00:20:53 -0500

    ahmad store update for filters

--- src/lib/stores/profiles.ts
+++ src/lib/stores/profiles.ts
@@ -1,39 +1,65 @@
-import type { Profile } from "$lib/components/profile/mock"
-import type { SupabaseClient } from "@supabase/supabase-js"
-import { get, writable, type Writable } from "svelte/store"
+import type { Profile } from '$lib/components/profile/mock';
+import type { SupabaseClient } from '@supabase/supabase-js';
+import { get, writable, type Writable } from 'svelte/store';
 
 interface FilterOptions {
-    stacks: string[]
-    availibility: string[]
-    experiences: string[]
+	stacks: string[];
+	availibility: string[];
+	experiences: string[];
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createProfilesState = (init: Profile[], supabase: SupabaseClient) => {
-    const initital = init
-    const profiles = writable(initital)
-    const filter: Writable<FilterOptions> = writable({ stacks: [], availibility: [], experiences: [] })
-    const sort = writable('')
+	const initital = init;
+	const profiles = writable(initital);
+	const filteredProfiles = writable(initital);
+	const filter: Writable<FilterOptions> = writable({
+		stacks: [],
+		availibility: [],
+		experiences: []
+	});
+	const sort = writable('');
 
-    filter.subscribe(async (filter) => {
-        const s = get(sort)
-        //TODO add both filter and sort constraints
-        // const { data } = await supabase.from('profiles').select()
-        // profiles.set(data as Profile[])
-    })
+	filter.subscribe((newFilters) => {
+		filteredProfiles.set(
+			get(profiles).filter((profile) => {
+				if (
+					newFilters.availibility.length &&
+					!profile.availabilities.some((availability) =>
+						newFilters.availibility.includes(availability)
+					)
+				)
+					return false;
 
-    sort.subscribe(async (sort) => {
-        const f = get(filter)
-        //TODO add both filter and sort constraints
-        // const { data } = await supabase.from('profiles').select()
-        // profiles.set(data as Profile[])
+				if (
+					!profile.projects
+						.map((project) =>
+							project.usedTech.some((experience) => newFilters.experiences.includes(experience))
+						)
+						.flat()
+						.some((bool) => bool)
+				)
+					return false;
 
-    })
+				if (
+					newFilters.experiences.length &&
+					!profile.projects.some((project) =>
+						project.usedTech.some((experience) => newFilters.experiences.includes(experience))
+					)
+				)
+					return false;
 
-    return {
-        profiles,
-        filter,
-        sort
-    }
-}
+				return true;
+			})
+		);
+	});
 
-export type CreateProfilesState = typeof createProfilesState
+	return {
+		profiles,
+		filteredProfiles,
+		filter,
+		sort
+	};
+};
+
+export type CreateProfilesState = typeof createProfilesState;
