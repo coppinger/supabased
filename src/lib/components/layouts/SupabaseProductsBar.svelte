d11fde2a12ebac60c440563d9d19e2044406b1e6
Finn Lawrence <finn.lawrence@gmail.com>
2024-04-22 17:13:31 +1200

    Make top bar dynamic from the db

--- src/lib/components/layouts/SupabaseProductsBar.svelte
+++ src/lib/components/layouts/SupabaseProductsBar.svelte
@@ -1,38 +1,33 @@
 <script lang="ts">
+	export let supabase;
+
 	import { Box, Cloud, Database, Lock, MousePointerClick, Triangle } from 'lucide-svelte';
-	const supabaseProducts = [
-		{
-			label: 'Database',
-			Icon: Database
-		},
-		{
-			label: 'Auth',
-			Icon: Lock
-		},
-		{
-			label: 'Storage',
-			Icon: Cloud
-		},
-		{
-			label: 'Edge',
-			Icon: Triangle
-		},
-		{
-			label: 'Realtime',
-			Icon: MousePointerClick
-		},
-		{
-			label: 'Vector',
-			Icon: Box
-		}
-	];
+
+	const supabaseProducts = supabase.from('supabase_products').select().order('sort');
+
+	const supabaseProductIcons = {
+		Database: Database,
+		Auth: Lock,
+		Storage: Cloud,
+		Edge: Triangle,
+		Realtime: MousePointerClick,
+		Vector: Box
+	};
 </script>
 
 <div class="border-b border-neutral-800 p-4 flex items-center justify-between flex-wrap gap-4">
-	{#each supabaseProducts as { label, Icon }}
-		<div class="flex gap-4 items-center text-emerald-400 font-bold">
-			{label}
-			<Icon class="h-4 w-4" />
-		</div>
-	{/each}
+	{#await supabaseProducts}
+		<div>Loading...</div>
+	{:then data}
+		{#each data.data as { name }}
+			<div class="px-1 flex gap-4 items-center text-emerald-400 font-bold">
+				{name}
+				{#if supabaseProductIcons[name]}
+					<svelte:component this={supabaseProductIcons[name]} class="h-4 w-4" />
+				{/if}
+			</div>
+		{/each}
+	{:catch error}
+		{console.log(error)}
+	{/await}
 </div>
