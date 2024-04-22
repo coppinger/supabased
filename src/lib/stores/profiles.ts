8e480edb7c4139ef4fdca06af338820af3ec2a2e
Charlie Coppinger <charles.coppinger@gmail.com>
2024-04-22 18:37:31 +1200

    4 dudes 1 vs code

--- src/lib/stores/profiles.ts
+++ src/lib/stores/profiles.ts
@@ -1,39 +1,75 @@
-import type { Profile } from "$lib/components/profile/mock"
-import type { SupabaseClient } from "@supabase/supabase-js"
-import { get, writable, type Writable } from "svelte/store"
+import type { Profile } from '$lib/components/profile/mock';
+import type { SupabaseClient } from '@supabase/supabase-js';
+import { get, writable, type Writable } from 'svelte/store';
 
 interface FilterOptions {
-    stacks: string[]
-    availibility: string[]
-    experiences: string[]
+	stacks: string[];
+	availibility: string[];
+	experiences: string[];
 }
 
 export const createProfilesState = (init: Profile[], supabase: SupabaseClient) => {
-    const initital = init
-    const profiles = writable(initital)
-    const filter: Writable<FilterOptions> = writable({ stacks: [], availibility: [], experiences: [] })
-    const sort = writable('')
+	const initital = init;
+	const profiles = writable(initital);
+	const filter: Writable<FilterOptions> = writable({
+		stacks: [],
+		availibility: [],
+		experiences: []
+	});
+	const sort = writable('');
+	const filteredProfiles = writable(initital);
 
-    filter.subscribe(async (filter) => {
-        const s = get(sort)
-        //TODO add both filter and sort constraints
-        // const { data } = await supabase.from('profiles').select()
-        // profiles.set(data as Profile[])
-    })
+	filter.subscribe(async (filter) => {
+		const s = get(sort);
+		//TODO add both filter and sort constraints
+		// const { data } = await supabase
+		// 	.from('profiles')
+		// 	.select('*')
+		// .profiles.set(data as Profile[]);
+	});
 
-    sort.subscribe(async (sort) => {
-        const f = get(filter)
-        //TODO add both filter and sort constraints
-        // const { data } = await supabase.from('profiles').select()
-        // profiles.set(data as Profile[])
+	sort.subscribe(async (sort) => {
+		// const f = get(filter);
+		// //TODO add both filter and sort constraints
+		// const { data } = await supabase.from('profiles').select();
+		// profiles.set(data as Profile[]);
+	});
+	filter.subscribe((newFilters) => {
+		// filteredProfiles.set(
+		// 	get(profiles).filter((profile) => {
+		// 		if (
+		// 			newFilters.availibility.length &&
+		// 			!profile.availabilities.some((availability) =>
+		// 				newFilters.availibility.includes(availability)
+		// 			)
+		// 		)
+		// 			return false;
+		// 		if (
+		// 			!profile.projects
+		// 				.map((project) =>
+		// 					project.usedTech.some((experience) => newFilters.experiences.includes(experience))
+		// 				)
+		// 				.flat()
+		// 				.some((bool) => bool)
+		// 		)
+		// 			return false;
+		// 		if (
+		// 			newFilters.experiences.length &&
+		// 			!profile.projects.some((project) =>
+		// 				project.usedTech.some((experience) => newFilters.experiences.includes(experience))
+		// 			)
+		// 		)
+		// 			return false;
+		// 		return true;
+		// 	})
+		// );
+	});
 
-    })
+	return {
+		profiles,
+		filter,
+		sort
+	};
+};
 
-    return {
-        profiles,
-        filter,
-        sort
-    }
-}
-
-export type CreateProfilesState = typeof createProfilesState
+export type CreateProfilesState = typeof createProfilesState;
---
+++ src/routes/onboarding/profile/profileSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+import { imageUploadSchema } from '../imageUploadSchema';
+
+export const profileSchema = z.object({
+	username: z.string().min(3).max(32),
+	display_name: z.string().min(3).max(64),
+	bio: z.string().max(160),
+	pfp_url: imageUploadSchema,
+	geopgrahical_location: z.string().default('Earth'),
+	dream: z.string().max(160)
+});
+
+export type Profile = z.infer<typeof profileSchema>;
