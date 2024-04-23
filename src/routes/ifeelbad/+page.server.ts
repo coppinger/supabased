2bede236c7c0b2e4aaf74f00b4dee05c898db712
Charlie Coppinger <charles.coppinger@gmail.com>
2024-04-23 13:04:48 +1200

    chip away at ProfileListItem component

--- src/routes/ifeelbad/+page.server.ts
+++ src/routes/ifeelbad/+page.server.ts
@@ -1,23 +1,31 @@
+import type { Tables } from '$lib/types/DatabaseDefinitions.js';
+
 export async function load({ locals: { supabase } }) {
 	// const { data } = await supabase.from('table').select();
 
 	//FIXME DELETEME when we have actual profile data
-	const profiles = await supabase.from('profiles').select(`
-	    *,
-	    profile_availability_types(
-	      *,
-	      availability_types (id, name)
-	    ),
-	    profiles_roles(
-	      *,
-	      roles (id, name)
-	    ),
-	    projects(
-	      *,
-	      projects_stacks(stack_id)
-	    ),
-	  `);
+	// const profiles = await supabase.from('profiles').select(`
+	//     *,
+	//     profile_availability_types(
+	//       *,
+	//       availability_types (id, name)
+	//     ),
+	//     profiles_roles(
+	//       *,
+	//       roles (id, name)
+	//     ),
+	//     projects(
+	//       *,
+	//       projects_stacks(stack_id)
+	//     ),
+	//   `);
+
+	const { data: profilesData } = await supabase
+		.from('profiles')
+		.select('*')
+		.returns<Tables<'profiles'>[]>();
+
 	return {
-		profiles
+		profilesData
 	};
 }
