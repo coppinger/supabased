bca7d54dd48da7aea4bc64082020056c2f0edc3b
Charlie Coppinger <charles.coppinger@gmail.com>
2024-04-24 18:09:13 +1200

    hello chat

--- supabase/migrations/20240421040156_create_projects.sql
+++ supabase/migrations/20240421040156_create_projects.sql
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-CREATE TABLE
-    "projects" (
-        "id" uuid PRIMARY KEY DEFAULT gen_random_uuid (),
-        "profile_id" uuid REFERENCES profiles (id),
-        "project_name" text,
-        "project_url" text,
-        "repository_url" text,
-        "description" text,
-        "created_at" timestamptz DEFAULT now (),
-        "updated_at" timestamptz,
-        "deleted_at" timestamptz
-    );
-
--- Set up Row Level Security
-ALTER TABLE projects ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Projects are viewable by everyone." ON projects FOR
-SELECT
-    USING (TRUE);
-
-CREATE POLICY "Users can insert their own projects." ON projects FOR INSERT
-WITH
-    CHECK (auth.uid () = profile_id);
-
-CREATE POLICY "Users can update their own projects." ON projects FOR
-UPDATE USING (auth.uid () = profile_id);
-
--- Set up Triggers
----- Set the `updated_at` column on every update
-CREATE TRIGGER handle_project_updated_at BEFORE
-=======
 CREATE TABLE
     "projects" (
         "id" uuid PRIMARY KEY DEFAULT gen_random_uuid (),
@@ -72,5 +40,4 @@ UPDATE USING (
 -- Set up Triggers
 ---- Set the `updated_at` column on every update
 CREATE TRIGGER handle_project_updated_at BEFORE
->>>>>>> master
 UPDATE ON projects FOR EACH ROW EXECUTE PROCEDURE moddatetime (updated_at);
--- supabase/migrations/20240421042759_create_supabase_products.sql
+++ supabase/migrations/20240421042759_create_supabase_products.sql
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-CREATE TABLE
-    "supabase_products" (
-        "id" uuid PRIMARY KEY DEFAULT gen_random_uuid (),
-        "name" text,
-        "sort" int
-    );
-
--- Set up RLS
-ALTER TABLE supabase_products ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Supabase products are viewable by everyone." ON supabase_products FOR
-SELECT
-    USING (TRUE);
-
--- Create join table
-CREATE TABLE
-    "supabase_products_projects" (
-        "id" uuid PRIMARY KEY DEFAULT gen_random_uuid (),
-        "supabase_product_id" uuid REFERENCES supabase_products (id),
-        "project_id" uuid REFERENCES projects (id)
-    );
-
--- Set up RLS
-ALTER TABLE supabase_products_projects ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY "Supabase products projects are viewable by everyone." ON supabase_products_projects FOR
-SELECT
-    USING (TRUE);
-
-CREATE POLICY "Profiles can insert into supabase_products_projects for their own projects" ON supabase_products_projects FOR INSERT
-WITH
-    CHECK (
-        auth.uid () = (
-            SELECT
-                profile_id
-            FROM
-                projects
-            WHERE
-                id = project_id
-        )
-    );
-
-CREATE POLICY "Profiles can update their own supabase_products_projects" ON supabase_products_projects FOR
-UPDATE USING (
-    auth.uid () = (
-        SELECT
-            profile_id
-        FROM
-            projects
-        WHERE
-            id = project_id
-    )
-=======
 CREATE TABLE
     "supabase_products" (
         "id" uuid PRIMARY KEY DEFAULT gen_random_uuid (),
@@ -114,5 +60,4 @@ UPDATE USING (
         WHERE
             id = project_id
     )
->>>>>>> master
 );
