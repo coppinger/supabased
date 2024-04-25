0728443d04aeaa6f2c3dc275b623c302f6d1c845
Finn Lawrence <finn.lawrence@gmail.com>
2024-04-25 16:28:29 +1200

    Export interface

--- src/lib/stores/profiles.ts
+++ src/lib/stores/profiles.ts
@@ -3,10 +3,10 @@ import { derived, get, writable, type Writable } from 'svelte/store'
 import { PROFILE_QUERY, type ProfilesResult } from '$lib/db/query'
 import type { Tables } from '$lib/types/DatabaseDefinitions'
 
-interface FilterOptions {
 	stacks: string[]
 	availibility: string[]
 	experiences: string[]
+export interface FilterOptions {
 }
 
 export const createProfilesState = (init: PostgrestSingleResponse<ProfilesResult[]>, supabase: SupabaseClient) => {
